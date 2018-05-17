69da6496ad0f7a4758e6ac8a68e23a89b23ec8f0
DMFletcher <thanatos.02@gmail.com>
2018-05-16 19:28:18 -0500

    resolvinc conflicts

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Root",
+    password: "Chronos02",
     database: "hngplants_db"
 });
 
