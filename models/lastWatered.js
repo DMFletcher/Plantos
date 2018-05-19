eb329336b94154e06ca3dd58dd79aec8469546df
DMFletcher <thanatos.02@gmail.com>
2018-05-18 20:25:59 -0500

    fixing bugs

--- models/lastWatered.js
+++ models/lastWatered.js
@@ -1,28 +1,28 @@
-module.exports = function (sequelize, DataTypes) {
-    var lastWatered = sequelize.define("lastWatered", {
-// lwd = last watered date
-        lwd1: {
-            type: DataTypes.DATEONLY,
-            allowNull: true
-        },lwd2: {
-            type: DataTypes.DATEONLY,
-            allowNull: true
-        },lwd3: {
-            type: DataTypes.DATEONLY,
-            allowNull: true
-        },lwd4: {
-            type: DataTypes.DATEONLY,
-            allowNull: true
-        }
-    });
+// module.exports = function (sequelize, DataTypes) {
+//     var lastWatered = sequelize.define("lastWatered", {
+// // lwd = last watered date
+//         lwd1: {
+//             type: DataTypes.DATEONLY,
+//             allowNull: true
+//         },lwd2: {
+//             type: DataTypes.DATEONLY,
+//             allowNull: true
+//         },lwd3: {
+//             type: DataTypes.DATEONLY,
+//             allowNull: true
+//         },lwd4: {
+//             type: DataTypes.DATEONLY,
+//             allowNull: true
+//         }
+//     });
 
-    lastWatered.associate = function (models) {
-        // console.log(models.userPlants);
-        console.log(models.Plant);
+//     lastWatered.associate = function (models) {
+//         // console.log(models.userPlants);
+//         console.log(models.Plant);
 
-        lastWatered.belongsTo(models.User, { through: models.Plant });
-        lastWatered.belongsTo(models.Plant);
-    };
+//         lastWatered.belongsTo(models.User, { through: models.Plant });
+//         lastWatered.belongsTo(models.Plant);
+//     };
 
-    return lastWatered;
-}; 
+//     return lastWatered;
+// }; 
